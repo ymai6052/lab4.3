d984ef463c14efb87d503bc8ca248fdaa332f181
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-01-09 12:43:31 -0500

    ggfg

--- lab4.3/src/easygame.java
+++ lab4.3/src/easygame.java
@@ -12,15 +12,51 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Group;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
  
 public class easygame extends Application {
 	private static int counter;
-    private static boolean shown;
-    public static void main(String[] args) {
-    	PrintWriter pw = null;
+    private boolean shown;
+    private static String name;
+    private static String sb;
+    private static PrintWriter pw;
+    private static String boardlist; 
+    public static void main(String[] args) throws IOException {
+    	System.out.println("Hello there what should I call you?");
+    	Scanner sc = new Scanner(System.in);
+        String i = sc.next();
+        name=i;
+       
+        System.out.println("High Score List");
+    	String csvFile ="results.csv";
+    	BufferedReader br = null;
+    	br = new BufferedReader(new FileReader(csvFile));
+    	String line="";
+    	
+    	while ((line = br.readLine()) != null)
+    	{
+
+            // use comma as separator
+            String[] board = line.split(",");
+            boardlist+=line+"\n";
+            System.out.println(board[0]+ "  "+board[1]);
+
+        }
+
+    	
+    	/*sb = new StringBuilder();
+    	sb.append("Name,High Score\n");
+    	sb.append("Derek,60\n");
+    	sb.append("Yiren,50\n");
+    	sb.append("Other,40\n");
+    	*/
     	try 
     	{
     		pw = new PrintWriter(new File("results.csv"));
@@ -29,13 +65,7 @@ public class easygame extends Application {
     	{
     		System.err.println(e);
     	}
-    	StringBuilder sb = new StringBuilder();
-    	sb.append("Name,High Score\n");
-    	sb.append("Derek,60\n");
-    	sb.append("Yiren,50\n");
-    	sb.append("Other,40\n");
-    	pw.write(sb.toString());
-    	pw.close();
+    	 System.out.println("How many times can u click the button in 10 seconds?");
     	counter=0;
         launch(args);
     }
@@ -59,7 +89,12 @@ public class easygame extends Application {
         	else if(shown==false)
         	{
         		shown=true;
-        		System.out.println(counter);
+        		System.out.println("In 10 seconds you have clicked it "+counter+ " times");
+        		sb=name+","+ Integer.toString(counter)+"\n";
+        		boardlist+=sb;
+        		pw.write(boardlist);
+            	pw.close();
+            	
         	}
         });
         	StackPane root = new StackPane();
